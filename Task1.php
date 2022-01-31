1fc03190e319deb3b4702813a46c27204bdef99c
Afrin Jassi <98738748+sumaiyaafrinjassi@users.noreply.github.com>
2022-01-31 17:38:35 +0600

    Add files via upload

---
+++ Task1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Task 1</title>
+</head>
+<body>
+    
+
+<h1> Registration Form </h1>
+<hr> </hr>
+<table>
+<tr>
+<td> First Name :</td>
+   <td>  <input type = "text" Name ="name"> </td>
+</tr>
+
+<tr>
+   <td> Last Name : </td>
+   <td> <input type = "text" Name = "name"></td>
+</tr>
+
+<tr>
+   <td> Age : </td>
+   <td> <input type="text" Name = "name"> </td>
+</tr>
+
+<tr>
+    <td>Designation:</td>
+    <td> 
+        <input type="radio" Name="desig">Junior Programmer
+        <input type="radio" Name="desig">Senior programmer
+        <input type="radio" Name="desig">Project Lead
+</td>
+</tr>
+
+<tr>
+    <td>Preferred Language:</td>
+    <td>
+        <input type="radio" Name="desig">JAVA
+        <input type="radio" Name="desig">PHP
+        <input type="radio" Name="desig">C++
+</td>
+</tr>
+
+<tr>
+<td> Email :</td>
+   <td>  <input type = "text"> </td>
+</tr>
+
+<tr>
+<td> Password :</td>
+   <td>  <input type = "text"> </td>
+</tr>
+
+<tr>
+    <td> Please choose a file</td>
+    <td>
+        <input type="file" id = "myfile"></td>
+</tr>
+</table>
+<input type="submit" name="submit">
+<input type="reset" name="reset">
+</body>
+</html>
